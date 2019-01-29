7b705269b115b0a7fb9b99e71ff6f09b0a67e250
Jaimy Magalhaes Coelho <35726400+mokin009x@users.noreply.github.com>
2019-01-29 12:35:48 +0100

    lesson 1 module 1

---
+++ Assets/Inventory.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public static Inventory instance;
+    public float TotalWeight;
+
+    void Awake()
+    {
+        if (Inventory.instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+}
